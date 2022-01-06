94bb1947351bd907e1629d63f152bdc114ea2682
Patrick Spafford <pas0037@auburn.edu>
2022-01-06 14:46:15 -0600

    Change initialization of window width for mobile

--- src/hooks/useWindowWidth/useWindowWidth.ts
+++ src/hooks/useWindowWidth/useWindowWidth.ts
@@ -1,11 +1,14 @@
 import { useTailwindTheme } from '@hooks'
 import { useState, useEffect } from 'react'
 import { useWindowSize as useWindowSizeClient } from 'rooks'
+import { isBrowser } from 'react-device-detect'
 
 const useWindowWidth = () => {
   const theme = useTailwindTheme()
   const windowSizeClient = useWindowSizeClient()
-  const [width, setWidth] = useState(theme.breakpoints.lg + 10)
+  const [width, setWidth] = useState(
+    isBrowser ? theme.breakpoints.lg + 10 : theme.breakpoints.lg - 10,
+  )
 
   useEffect(() => {
     if (windowSizeClient.innerWidth !== 0) {
