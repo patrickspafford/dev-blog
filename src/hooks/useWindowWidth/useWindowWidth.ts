beb13648c5d43b2de2a3d2eb6a8d36dd31d93be3
Patrick Spafford <pas0037@auburn.edu>
2022-01-08 23:05:39 -0600

    Make sure window width runs on client side

--- src/hooks/useWindowWidth/useWindowWidth.ts
+++ src/hooks/useWindowWidth/useWindowWidth.ts
@@ -1,17 +1,12 @@
-import { useTailwindTheme } from '@hooks'
 import { useState, useEffect } from 'react'
 import { useWindowSize as useWindowSizeClient } from 'rooks'
-import { isBrowser } from 'react-device-detect'
 
 const useWindowWidth = () => {
-  const theme = useTailwindTheme()
   const windowSizeClient = useWindowSizeClient()
-  const [width, setWidth] = useState(
-    isBrowser ? theme.breakpoints.lg + 10 : theme.breakpoints.lg - 10,
-  )
+  const [width, setWidth] = useState(0)
 
   useEffect(() => {
-    if (windowSizeClient.innerWidth !== 0) {
+    if (windowSizeClient.innerWidth !== 0 && typeof window !== 'undefined') {
       setWidth(windowSizeClient.innerWidth)
     }
   }, [windowSizeClient.innerWidth])
