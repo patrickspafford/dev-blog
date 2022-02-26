98110c3c27548f9fe1f0821249fbdfa83b724797
Patrick Spafford <pas0037@auburn.edu>
2022-02-26 02:01:41 -0600

    Fix scroll progress position

--- src/components/HeaderContainer/HeaderContainer.tsx
+++ src/components/HeaderContainer/HeaderContainer.tsx
@@ -7,7 +7,7 @@ const HeaderContainer = ({ children }: IChildren) => {
   return (
     <header className="w-full h-24 gradient dark:gradient-dark dark:border-b dark:border-gray-300 sticky shadow-lg flex items-stretch justify-between pl-2 pr-2 top-0 left-0 right-0 z-50">
       {children}
-      <div className="absolute bottom-0 left-0 right-0 h-1 bg-white dark:bg-gray-300">
+      <div className="absolute -bottom-px left-0 right-0 h-1 bg-white dark:bg-gray-300">
         <div
           className="bg-pink h-1 transition-width ease-linear duration-300"
           style={{ width: `${progress}%` }}
