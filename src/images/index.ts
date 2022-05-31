296b1d396692c60735097c4fd51a57e51f54f0c7
Patrick Spafford <pas0037@auburn.edu>
2022-05-30 21:05:31 -0700

    Remove unused images

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,30 +1,10 @@
 import P from './P.png'
-import Galaxy from './galaxy.png'
-import Ocean from './ocean.png'
-import Rainier from './rainier.webp'
 import Me from './me.jpg'
 import Pug from './pug.png'
-import Muffin from './muffin.png'
 import Eth from './eth.png'
 import Golang from './golang.png'
-import Mountain from './mountain.webp'
 import RainierWide from './rainier-wide.webp'
 import ReactNative from './react-native.png'
 import Solidity from './solidity.png'
 import SwiftUI from './swift-ui.png'
-export {
-  P,
-  Galaxy,
-  Ocean,
-  Rainier,
-  Me,
-  Pug,
-  Muffin,
-  Eth,
-  Golang,
-  Mountain,
-  RainierWide,
-  ReactNative,
-  Solidity,
-  SwiftUI,
-}
+export { P, Me, Pug, Eth, Golang, RainierWide, ReactNative, Solidity, SwiftUI }
