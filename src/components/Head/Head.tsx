ae761f7f3f8e16e29d74feaffc171b9285912f9e
Patrick Spafford <pas0037@auburn.edu>
2022-06-16 19:44:01 -0700

    Remove google ad script

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Script } from 'gatsby'
 import { Helmet } from 'react-helmet'
 import { IHead } from './types'
 import useHeadQuery from './useHeadQuery'
@@ -9,11 +8,6 @@ const Head = ({ pageTitle }: IHead) => {
   return (
     <head>
       <Helmet title={`${pageTitle} | ${data.site.siteMetadata.title}`} />
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8762746208937385"
-        crossOrigin="anonymous"
-      />
     </head>
   )
 }
